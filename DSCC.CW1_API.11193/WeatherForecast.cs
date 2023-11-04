91839ebab4af7636059ee4b71d033ee8ed0f83a8
wiut00011193 <wiut00011193@gmail.com>
2023-11-04 15:45:44 +0500

    first commit

---
+++ DSCC.CW1_API.11193/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace DSCC.CW1_API._11193
+{
+    public class WeatherForecast
+    {
+        public DateOnly Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
